61baac3a8292f7cd2e3e64f45c05ffa62cda3115
Batajoo <sanjivbatajoo@gmail.com>
2025-01-09 23:32:34 -0600

    Header component created

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import { Footer, Header } from './components/index'
+
+function App() {
+
+  return (
+    <>
+      <Header/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
